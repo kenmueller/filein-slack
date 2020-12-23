727bb94679b2407b61aeb0e6ddf00fb97dd4c125
Ken Mueller <kenmueller0@gmail.com>
2020-12-23 04:48:24 -0800

    test message event

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,14 @@ const app = new App({
 	token: process.env.SLACK_TOKEN
 })
 
+app.event('message', async ({ event }) => {
+	await app.client.chat.postMessage({
+		channel: event.channel,
+		thread_ts: event.ts,
+		text: JSON.stringify(event)
+	})
+})
+
 app.start(PORT).then(() => {
 	console.log(`Listening on port ${PORT}`)
 })
