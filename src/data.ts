46cce435aec0bf01e025994b9cf05c9827d45745
Ken Mueller <kenmueller0@gmail.com>
2020-12-23 16:07:37 -0800

    testing

--- src/data.ts
+++ src/data.ts
@@ -4,7 +4,7 @@ import File from './file'
 
 const AUTHORIZATION = `Bearer ${process.env.SLACK_TOKEN}`
 
-const getData = async (file: File) => {
+const getData = async (file: File): Promise<Buffer> => {
 	const response = await fetch(file.url_private, {
 		headers: { Authorization: AUTHORIZATION }
 	})
@@ -12,11 +12,13 @@ const getData = async (file: File) => {
 	if (!response.ok)
 		throw new Error('An error occurred when loading the file')
 	
-	try {
-		return response.buffer()
-	} catch {
-		throw new Error(JSON.stringify(await response.json()))
-	}
+	throw new Error(JSON.stringify(await response.json()))
+	
+	// try {
+	// 	return response.buffer()
+	// } catch {
+	// 	throw new Error(JSON.stringify(await response.json()))
+	// }
 }
 
 export default getData
